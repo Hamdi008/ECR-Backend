764d5c81b64ad40c5d924a51ed8f1cbbbc569dc4
helfekih <hamdi.elfekih@groupe-telnet.net>
2024-08-08 14:51:43 +0100

    Create addUser API

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,18 @@ const User = require("../models/user");// import the User model
 
 const router = express.Router();
 
+//addUser API
+router.post("/addUser", async(req,res)=>{
+    try {
+
+        data = req.body;
+        usr = new User(data)
+        savedData = await usr.save();
+        res.status(200).send(savedData)
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');// import express framework
 require("./config/connect");//import connect.js
+const userRouter = require("./routes/user")
 
 const app = express();
 // this is to allow the server to read json data
 app.use(express.json());
+
+//http://127.0.0.1:3000/user/
+app.use("/user", userRouter);
 
 app.listen( 3000, ()=>{
     console.log("server running...")
