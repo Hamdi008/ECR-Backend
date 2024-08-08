6cad34bb140cc8d14309e7d4722b1342f797c63a
helfekih <hamdi.elfekih@groupe-telnet.net>
2024-08-08 21:03:55 +0100

    update user model and Use CORS middleware

--- models/user.js
+++ models/user.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose")
 
 const User = mongoose.model("User",{
-    firstname:{
+    fullname:{
         type: String
-    },
-    lastname:{
-        type:String
     },
     email:{
         type: String
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');// import express framework
 require("./config/connect");//import connect.js
 const userRouter = require("./routes/user")
+const cors = require('cors');  // Import the cors package
 
 const app = express();
 // this is to allow the server to read json data
 app.use(express.json());
+
+// Use CORS middleware to allow requests from specific origins
+app.use(cors({
+    origin: 'http://localhost:4200',  // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  }));
 
 //http://127.0.0.1:3000/user/
 app.use("/user", userRouter);
