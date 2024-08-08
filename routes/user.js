f30b9c8e254865ae803203f897b48805258c3c95
helfekih <hamdi.elfekih@groupe-telnet.net>
2024-08-08 15:59:22 +0100

    Add CRUD APIs

--- routes/user.js
+++ routes/user.js
@@ -17,4 +17,52 @@ router.post("/addUser", async(req,res)=>{
     }
 });
 
+//getUsers API
+router.get("/getUsers", async(req,res)=>{
+    try {
+        users = await User.find()
+        res.status(200).send(users)
+        
+    } catch (error) {
+     res.status(400).send(error) 
+    }
+});
+
+//getUserById
+router.get("/getUserById/:id", async(req,res)=>{
+
+    try {
+        myId = req.params.id;
+        user = await User.findOne({_id: myId});
+        res.status(200).send(user);
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+
+//updateUserById
+router.put("/updateUserById/:id", async(req,res)=>{
+
+    try {
+        myId = req.params.id;
+        newData = req.body;
+        updatedData = await User.findByIdAndUpdate({_id: myId}, newData);
+        res.status(200).send(updatedData);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+//deleteUserById
+router.delete("/deleteUserById/:id", async(req,res)=>{
+    try {
+        myId = req.params.id;
+        deletedUser = await User.findOneAndDelete({_id: myId});
+        res.status(200).send(deletedUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
